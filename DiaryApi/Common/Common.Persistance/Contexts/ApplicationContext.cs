0d9553170a1456dfbbda8edece8f750f8ee160f0
Markevych Oleh <omarkevych@leobit.com>
2019-11-16 20:29:47 +0200

    Install EntityFramework.Core, add repository pattern and database context

---
+++ DiaryApi/Common/Common.Persistance/Contexts/ApplicationContext.cs
@@ -0,0 +1,18 @@
+﻿using Common.Persistence.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Common.Persistence.Contexts
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Group> Groups { get; set; }
+        public DbSet<GroupSubject> GroupSubjects { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<StudentResult> StudentResults { get; set; }
+        public DbSet<Subject> Subjects { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+        public DbSet<TeacherSubject> TeacherSubjects { get; set; }
+        public DbSet<Topic> Topics { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ DiaryApi/Common/Common.Persistance/Repositories/GenericRepository.cs
@@ -0,0 +1,74 @@
+﻿using Common.Domain.Interfaces.Persistance;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Common.Persistence.Repositories
+{
+    public class GenericRepository<TEntity> : IRepository<TEntity> 
+        where TEntity : class
+    {
+        DbContext context;
+        DbSet<TEntity> dbSet;
+
+        public GenericRepository(DbContext context)
+        {
+            this.context = context;
+            this.dbSet = context.Set<TEntity>();
+        }
+
+        public IEnumerable<TEntity> Get()
+        {
+            return this.dbSet.AsNoTracking().ToList();
+        }
+
+        public IEnumerable<TEntity> Get(Func<TEntity, bool> predicate)
+        {
+            return this.dbSet.AsNoTracking().Where(predicate).ToList();
+        }
+
+        public TEntity FindById(int id)
+        {
+            return this.dbSet.Find(id);
+        }
+
+        public void Create(TEntity item)
+        {
+            this.dbSet.Add(item);
+            this.context.SaveChanges();
+        }
+
+        public void Update(TEntity item)
+        {
+            this.context.Entry(item).State = EntityState.Modified;
+            this.context.SaveChanges();
+        }
+
+        public void Remove(TEntity item)
+        {
+            this.dbSet.Remove(item);
+            this.context.SaveChanges();
+        }
+
+        public IEnumerable<TEntity> GetWithInclude(params Expression<Func<TEntity, object>>[] includeProperties)
+        {
+            return Include(includeProperties).ToList();
+        }
+
+        public IEnumerable<TEntity> GetWithInclude(Func<TEntity, bool> predicate,
+            params Expression<Func<TEntity, object>>[] includeProperties)
+        {
+            var query = Include(includeProperties);
+            return query.Where(predicate).ToList();
+        }
+
+        private IQueryable<TEntity> Include(params Expression<Func<TEntity, object>>[] includeProperties)
+        {
+            IQueryable<TEntity> query = this.dbSet.AsNoTracking();
+            return includeProperties
+                .Aggregate(query, (current, includeProperty) => current.Include(includeProperty));
+        }
+    }
+}
